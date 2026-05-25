e734541b8de61a5e398e0dd948ca077a5fc346b4
talles-dt <talles.tonatto@gmail.com>
2026-05-25 16:26:07 -0300

    fix(nextjs): add turbopack config to resolve Turbopack/webpack conflict

--- next.config.js
+++ next.config.js
@@ -22,6 +22,7 @@ const nextConfig = {
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "",
   },
+  turbopack: {},
 };
 
 module.exports = nextConfig;
