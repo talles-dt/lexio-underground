e50117e7b0821138a781284a472e3c658c778125
talles-dt <talles.tonatto@gmail.com>
2026-05-25 16:59:03 -0300

    fix(callback): use next/navigation for useRouter and useSearchParams in web build

--- app/(auth)/callback.tsx
+++ app/(auth)/callback.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, Text, ActivityIndicator } from "react-native";
-import { useRouter } from "expo-router";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { supabase } from '@/lib/supabase';
 
 export default function Callback() {
